f3ce458eb8d4d471ccc36d93fd0efbf946ef3919
Mitesh Tank <codewithmitesh@gmail.com>
2021-08-29 23:29:36 +0530

    Till lecture 12 Countinue From lecture 13

--- app/src/main/java/com/example/marvelnotes/NotesMain.kt
+++ app/src/main/java/com/example/marvelnotes/NotesMain.kt
@@ -9,6 +9,21 @@ class NotesMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_main)
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
 
 }
