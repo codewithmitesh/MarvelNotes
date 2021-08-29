540f61ebb7a2414239c1347ed30cbdabc65188c9
Mitesh Tank <codewithmitesh@gmail.com>
2021-08-29 15:40:24 +0530

    Adding 2nd Activity

--- app/src/main/java/com/example/marvelnotes/MainActivity.kt
+++ app/src/main/java/com/example/marvelnotes/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.marvelnotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun getNotes(view: View) {
+        val intent = Intent(this,NotesMain::class.java)
+        startActivity(intent)
     }
 }
